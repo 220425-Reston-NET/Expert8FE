7a2b5c9c8ddb710f6c7373d4aa8ba161e00f2084
cmunoz11 <claritzamunoz1991@gmail.com>
2022-07-01 14:39:32 -0500

    updated login and welcome and added nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './shared/Nav';
+import { Route, Routes } from 'react-router-dom';
+import Welcome from './routes/welcome/Welcome';
+import Login from './routes/login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav/>
+    <Routes>
+      <Route path='/' element={<Welcome/>}/>
+      <Route path='/login' element={<Login/>}/>
+      </Routes>
     </div>
   );
 }
--- src/routes/welcome/Welcome.tsx
+++ src/routes/welcome/Welcome.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Welcome() {
   return (
-    <div>Welcome</div>
+    <div>
+            <img src="C:\Users\clari\Documents\P3\expert-fe\logo_transparent.png"></img>
+    </div>
   )
 }
 
 export default Welcome
+
